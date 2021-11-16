ae9d52c0e07d36e8b51b248870ecacd774829fb2
Karol Adamiak <karol.kosciany@gmail.com>
2021-11-16 18:42:00 +0100

    webpage init version

---
+++ main.js
@@ -0,0 +1,9 @@
+      // When the user clicks on <div>, open the popup
+      function showCookiePolicy() {
+        const popup = document.getElementById("cookiePopup");
+        popup.classList.toggle("show");
+      }
+      function showPrivacyPolicy() {
+        const popup = document.getElementById("privacyPopup");
+        popup.classList.toggle("show");
+      }
