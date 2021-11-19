9c331f2ac866973ff37d6344b02b0fc5ade12398
Karol Adamiak <karol.kosciany@gmail.com>
2021-11-19 16:09:26 +0100

    webpage first draft

--- main.js
+++
@@ -1,9 +0,0 @@
-      // When the user clicks on <div>, open the popup
-      function showCookiePolicy() {
-        const popup = document.getElementById("cookiePopup");
-        popup.classList.toggle("show");
-      }
-      function showPrivacyPolicy() {
-        const popup = document.getElementById("privacyPopup");
-        popup.classList.toggle("show");
-      }
